5f8f6536a858d5d182537cdb4352b9ba39a083e9
Omar-Code-Coding <omaryasser_250@hotmail.com>
2022-11-06 15:33:41 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { argv } = require("process");
+
+const config = {
+  entry: "./src/js/index.js",
+
+  target: "web",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    open: {
+      app: {
+        name: "chrome",
+      },
+    },
+    port: 3000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "eval-source-map";
+  }
+
+  if (argv.mode === "production") {
+    config.devtool = "source-map";
+  }
+
+  return config;
+};
